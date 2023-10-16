7addc6acbc901fcdbc626eedaf476d9da71b23f6
MacMaster <andrew.macmaster@fiserv.com>
2023-10-16 15:53:48 -0400

    Added recursive FizzBuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -25,12 +25,41 @@ public class FizzBuzz
             System.out.print(" ");
         }
     }
+
+    public static String recursive_fizzbuzz(int n, int i, String result)
+    {
+        if (i <= n)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                result += "FizzBuzz";
+            }
+            else if (i % 3 == 0)
+            {
+                result += "Fizz";
+            }
+            else if (i % 5 == 0)
+            {
+                result += "Buzz";
+            }
+            else
+            {
+                result += i;
+            }
+            result += " ";
+            i++;
+            return recursive_fizzbuzz(n, i, result);
+        }
+        return result;
+        
+    }
     
     public static void main(String[] args)
     {
         Scanner console = new Scanner(System.in);
         System.out.print("Enter a number n > 1 to play Fizz Buzz: ");
         System.out.println();
-        FizzBuzz(console.nextInt());
+        //FizzBuzz(console.nextInt());
+        System.out.println(recursive_fizzbuzz(console.nextInt(), 1, ""));
     }
 }
